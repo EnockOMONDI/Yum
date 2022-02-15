abcafc08179d42c84094123c5f24327a7cbb76cf
enockomondi <djseanizellkenya>
2022-02-15 20:01:45 +0300

    first commit

---
+++ ecommerce/constants.py
@@ -0,0 +1,71 @@
+# JavaScript Libraries
+#
+
+
+
+AGENCY_JS_LIBRARY_URLS = [
+    "yummyjs/jquery/1.11.1/jquery.min.js",
+    "yummyjs/jquery/1.11.1/jquery.tablesorter.js",
+    "yummyjs/jquery/1.11.1/jquery-ui.js",
+    "yummyjs/jquery-easing/1.3/jquery.easing.min.js",
+    "yummyjs/bootstrap/3.3.2/js/bootstrap.min.js",
+    "yummyjs/bootstrap/3.3.2/js/bootstrap.js",
+    "yummyjs/classie/1.0.0/classie.js",
+    "yummyjs/cbpanimatedheader/1.0.0/cbpAnimatedHeader.js",
+    "yummyjs/cbpanimatedheader/1.0.0/cbpAnimatedHeader.min.js",
+    "yummyjs/jqbootstrapvalidation/1.3.6/jqBootstrapValidation.js",
+    "yummyjs/misc/agency.js",
+  
+]
+
+CSS_LIBRARY_URLS  = [
+                        " assets/css/libs.min.css",
+                        " assets/vendor/fortawesome/fontawesome-free/css/all.min.css",
+                        " assets/css/foodsto.mine209.css?v=1.0.0"
+                        " assets/css/foodsto.mine209.css?v=1.0.0"
+                      
+]  
+
+JS_LIBRARY_URLS = [
+                        "assets/js/libs.min.js",
+                        "assets/vendor/gsap/gsap.min.js",
+                        "assets/vendor/gsap/ScrollTrigger.min.js",
+                        "assets/js/app.js",
+                        "assets/js/gsap.js",
+                        "assets/js/slider.js"
+ ]
+SB_ADMIN_2_CSS_LIBRARY_URLS= [
+                            "yummycss/bootstrap.min.css",
+                            "yummycss/font.css",
+                            "yummycss/font-awesome.min.css",
+                            "yummycss/swiper.min.css",
+                            "yummycss/magnific-popup.css",
+                            "yummycss/layers.css",
+                            "yummycss/navigation.css",
+                            "yummycss/settings.css",
+                            "yummycss/range.css",
+                            "yummycss/nice-select.css",
+                            "yummycss/style.css",
+] 
+
+SB_ADMIN_2_JS_LIBRARY_URLS= [ 
+                            "yummyjs/jquery.js",
+                            "yummyjs/bootstrap.min.js",
+                            "yummyjs/swiper.min.js",
+                            "yummyjs/magnific-popup.min.js",
+                            "yummyjs/jquery.themepunch.tools.min.js",
+                            "yummyjs/jquery.themepunch.revolution.min.js",
+                            "yummyjs/jquery.appear.js",
+                            "yummyjs/jquery.countTo.js",
+                            "yummyjs/isotope.min.js",
+                            "yummyjs/nice-select.min.js",
+                            "yummyjs/range.js",
+                            "yummyjs/revolution.extension.actions.min.js",
+                            "yummyjs/revolution.extension.kenburn.min.js",
+                            "yummyjs/revolution.extension.layeranimation.min.js",
+                            "yummyjs/revolution.extension.migration.min.js",
+                            "yummyjs/revolution.extension.parallax.min.js",
+                            "yummyjs/revolution.extension.slideanims.min.js",
+                            "yummyjs/revolution.extension.video.min.js",
+                            "yummyjs/custom.js",
+]
